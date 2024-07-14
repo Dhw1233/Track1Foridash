72200845259bf1393ef6a5518f06bb888bd636e9
unknown <459346786@qq.com>
2024-07-14 13:45:22 +0800

    1

---
+++ CHALLENGE_DATA/DASHformer_Challenge.py
@@ -0,0 +1,243 @@
+######################################
+#
+# Permission is hereby granted by the iDASH Organizing Committee, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+######################################
+import os
+import sys
+import numpy as np
+import json
+import random
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Input, Dense, Dropout, LayerNormalization, Embedding
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
+from sklearn.model_selection import train_test_split
+
+class PositionalEncoding(tf.keras.layers.Layer):
+    def __init__(self, use_positional_encoding, max_seq_length, embed_dim):
+        super(PositionalEncoding, self).__init__()
+        self.pos_encoding = self.positional_encoding(max_seq_length, embed_dim)
+        self.max_seq_length = max_seq_length
+        self.embed_dim = embed_dim
+        self.use_positional_encoding = use_positional_encoding
+
+    def get_angles(self, pos, i, embed_dim):
+        angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(embed_dim))
+        return pos * angle_rates
+
+    def positional_encoding(self, position, embed_dim):
+        angle_rads = self.get_angles(np.arange(position)[:, np.newaxis], np.arange(embed_dim)[np.newaxis, :], embed_dim)
+        angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+        angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+        pos_encoding = angle_rads[np.newaxis, ...]
+        return tf.cast(pos_encoding, dtype=tf.float32)
+
+    def call(self, inputs):
+        if self.use_positional_encoding == 1:
+            return inputs + self.pos_encoding[:, :tf.shape(inputs)[1], :]
+        elif self.use_positional_encoding == 0:
+            print('***NOT USING POSITIONAL ENCODING***')
+            return inputs
+
+# Build the MH Self Attention.
+class MultiHeadSelfAttention(tf.keras.layers.Layer):
+    def __init__(self, embed_dim, num_heads):
+        print(f"Setting up {num_heads}-head self-attention layer with {embed_dim}-dimensional embeddings.")
+        super(MultiHeadSelfAttention, self).__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+
+        # Make sure the embedding dimension is separable among heads.
+        assert embed_dim % num_heads == 0
+
+        # Setup the Q, K, V matrices.
+        self.projection_dim = embed_dim // num_heads
+        self.query_dense = Dense(embed_dim)
+        self.key_dense = Dense(embed_dim)
+        self.value_dense = Dense(embed_dim)
+        self.combine_heads = Dense(embed_dim)
+
+    # Compute attention score.
+    def attention(self, query, key, value):
+        score = tf.matmul(query, key, transpose_b=True)
+        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
+        scaled_score = score / tf.math.sqrt(dim_key)
+        weights = tf.nn.softmax(scaled_score, axis=-1)
+        output = tf.matmul(weights, value)
+        return output, weights
+
+    # Separate the multi-head into individual heads.
+    def separate_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+
+        print('==========================================')
+        print(f'Projection dim: {self.projection_dim}; Query: {self.query_dense(inputs).shape}; Key: {self.key_dense(inputs).shape}; Value: {self.value_dense(inputs).shape}')
+
+        # Instantiate the QKV layers.
+        query = self.query_dense(inputs)
+        key = self.key_dense(inputs)
+        value = self.value_dense(inputs)
+
+        # Separate the heads.
+        query = self.separate_heads(query, batch_size)
+        key = self.separate_heads(key, batch_size)
+        value = self.separate_heads(value, batch_size)
+        print(f'Single head dimensions:  Query: {query.shape}; Key: {key.shape}; Value: {value.shape}')
+
+        # Calculate attention.
+        attention, weights = self.attention(query, key, value)
+
+        # Transpose the attention output.
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+
+        # Concatenate the attention.
+        concat_attention = tf.reshape(attention, (batch_size, -1, self.embed_dim))
+        output = self.combine_heads(concat_attention)
+
+        return output
+
+class TransformerBlock(tf.keras.layers.Layer):
+    def __init__(self, embed_dim, num_heads, attention_block_activation_type, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = MultiHeadSelfAttention(embed_dim, num_heads)
+
+        # Instantiate FFN.
+        self.ffn = tf.keras.Sequential([
+            Dense(ff_dim, name='ffn1', activation='relu'),
+            Dense(embed_dim, name='ffn2'),
+        ])
+                        
+        self.layernorm1 = LayerNormalization(epsilon=1e-6, name="LayerNorm1")
+        self.layernorm2 = LayerNormalization(epsilon=1e-6, name="LayerNorm2")
+        self.dropout1 = Dropout(rate, name='LayerDropout1')
+        self.dropout2 = Dropout(rate, name='LayerDropout2')
+
+    def call(self, inputs, training):
+        attn_output = self.att(inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+
+        ffn_output = self.ffn(out1)
+
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
+
+def create_transformer_model(max_seq_length, use_positional_encoding, vocab_size, embed_dim, num_heads, attention_block_activation_type, ff_dim, num_layers, output_dim, rate=0.1):
+    inputs = Input(shape=(max_seq_length,))
+    embedding_layer = Embedding(vocab_size, embed_dim)(inputs)
+    positional_encoding_layer = PositionalEncoding(use_positional_encoding, max_seq_length, embed_dim)(embedding_layer)
+    transformer_blocks = []
+    for _ in range(num_layers):
+        transformer_blocks.append(TransformerBlock(embed_dim, num_heads, attention_block_activation_type, ff_dim, rate))
+    transformer_output = positional_encoding_layer
+    for transformer_block in transformer_blocks:
+        transformer_output = transformer_block(transformer_output)
+    pool = tf.keras.layers.GlobalAveragePooling1D(name='GlobalPool_Transformers')(transformer_output)
+    dense = Dense(output_dim, activation="softmax", name='Dense_Classifier')(pool)
+    model = Model(inputs=inputs, outputs=dense)
+    return model
+
+#######################################################
+
+def main():
+
+    if len(sys.argv) < 2:
+        print(f"USAGE: {sys.argv[0]} [option] [arguments]\n\
+    -evaluate_DASHformer [Test sequences CSV] [Model .keras file] [Tokenizer file] [Max sequence length] [Predictions output file]\n")
+        sys.exit(1)
+
+    cmd_option = sys.argv[1]
+
+    # List the GPU devices, if any.
+    print(f"GPU devices: {tf.config.list_physical_devices('GPU')}")
+
+    if cmd_option == "-evaluate_DASHformer":
+        if len(sys.argv) != 7:
+            print(f'USAGE: {sys.argv[0]} {sys.argv[1]} [Test sequences CSV] [Model .keras file] [Tokenizer file] [Max sequence length] [Predictions output file]\n')
+            sys.exit(1)
+
+        protein_seq_file = sys.argv[2]
+        keras_model_file = sys.argv[3]
+        tokenizer_file = sys.argv[4]
+        max_seq_length = int(sys.argv[5])
+        output_probs_file = sys.argv[6]
+
+        if os.path.exists(protein_seq_file) == False:
+            print(f'Could not find sequences CSV file {protein_seq_file}')
+
+        if os.path.exists(keras_model_file) == False:
+            print(f'Could not keras model file {keras_model_file}')
+
+        # Assuming data_file format: sequence,class_label
+        sequences = []
+        class_labels = []
+
+        with open(protein_seq_file, 'r') as file:
+            for line in file:
+                sequence, label = line.strip().split(',')
+                sequences.append(sequence.split())  # Assuming sequences are whitespace-separated tokens
+                class_labels.append(int(label))  # Assuming class labels are integers
+
+        class_labels_onehot = to_categorical(class_labels)
+
+        print(f"Shape of one-hot encoded class labels: {class_labels_onehot.shape}")
+
+        # Define the classes while loading.
+        print(f'Loading model from {keras_model_file}')
+        model = load_model(keras_model_file, 
+                           custom_objects={"PositionalEncoding": PositionalEncoding,
+                                           "TransformerBlock": TransformerBlock,
+                                           "MultiHeadSelfAttention": MultiHeadSelfAttention})
+        
+        with open('model_summary.txt', 'w') as f:
+            model.summary(print_fn=lambda x: f.write(x + '\n'))
+
+        # Tokenization and Padding starts below..
+        print(f'Loading Tokenizer from {tokenizer_file}')
+        with open(tokenizer_file, 'r') as file:
+            tokenizer_config = file.read()
+
+        # Load tokenizer from JSON configuration
+        tokenizer = tokenizer_from_json(tokenizer_config)
+        sequences_tokenized = tokenizer.texts_to_sequences(sequences)
+        sequences_padded = pad_sequences(sequences_tokenized, maxlen=max_seq_length, padding='post')
+
+        # Write the information on vocabulary, etc.
+        vocab_size = len(tokenizer.word_index) + 1
+        output_dim = len(set(class_labels))
+        print(f"Vocab-size is {vocab_size}")
+
+        prediction_results = model.predict(sequences_padded)
+
+        eval_results = model.evaluate(sequences_padded, class_labels_onehot)
+
+        print("Loss:", eval_results[0])
+        print("Accuracy:", eval_results[1])
+
+        with open(output_probs_file, 'w') as file:
+            file.write("Sequence,Label,Prediction\n")
+            for sequence, label, prediction in zip(sequences, class_labels, prediction_results):
+                file.write(f"{sequence},{label},{np.argmax(prediction)}\n")
+
+        ###############################################################################################
+
+    
+if __name__ == '__main__':
+    main()
+
+########################################
